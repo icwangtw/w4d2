e15fa418a7241a891431a17a1280adc8fb21ca76
Stephen <stephenicwang@gmail.com>
2018-05-01 23:31:28 +0000

    added second migration file

---
+++ migrations/20180501232415_create_milestones_fk.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('milestones', function(table){
+      table.increments('id').primary();
+      table.string('description');
+      table.date('date_achieved');
+      table.integer('famous_person_id').references('id').inTable('famous_people').notNull().onDelete('cascade');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+return Promise.all([
+    knex.schema.dropTable('milestones')
+  ])
+};
